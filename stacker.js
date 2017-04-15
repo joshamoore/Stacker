fde464503a92a151b4aca98beb218028e41e4501
Josh Moore <joshmoore2@gmail.com>
2017-04-15 14:20:48 -0300

    Working version
    Fixed:
    -Blocks not spawning on current row
    
    Added:
    -Blocks are now removed if a block is not directly on top of another block
    -Game over message when blocks remaining is <=0
    -Cannot surpass the height of rows with blocks anymore

--- stacker.js
+++ stacker.js
@@ -2,12 +2,12 @@
 
 Stacker
 Author: Josh Moore
-Last modified: March 9, 2017
+Last modified: March 20, 2017
 
-Description: To win the game, simply create a stack of blocks to the top of
+Description: To win the game, create a stack of blocks to the top of
 the board to win a prize
 
-How it works: The stacker board is simply modelled as 2D matrix, which spawns
+How it works: The stacker board is modelled as 2D matrix, which spawns
 blocks on each row as the game is played. The board is refreshed 30 times per 
 second.
 
@@ -15,7 +15,10 @@ Dimensions of the canvas are set to width: 280 and height: 480
 the size of the block is 40x40, so this gives us a 7x12 grid
 
 */
-var speed = 200; // ms
+var win = false;
+var gameOver = false;
+
+var speed = 100; // ms
 var height = 12;
 var width = 7;
 var remaining = 3;	// the number of blocks that will spawn - decrements when a block is missed
@@ -25,11 +28,13 @@ var blockWidth = 40;
 var reverse = false;
 var currCol = 0;
 var currRow = 0;
-var togo = 2; 		// To count the blocks displayed when going into the rhs
-var togoleft = 0;	// To count the blocks displayed when going into the lhs
+var togo = 0; 		// To count the blocks displayed
+var temp = 0;
+
+var check = false;
 
 var saved = new Array();
-saved.length = 7*12;	// to initialize array to proper size
+saved.length = width*height;	// to initialize array to proper size
 
 // Some simple test values for saved
 
@@ -47,16 +52,31 @@ document.addEventListener('keydown', (event) => {
   // Save the game if it was pressed
   if (keyName == 32) {
   	console.log("Saving game!");
-  	// update position
-  	currRow++;
-  	currCol=0;
-  	togoleft=0;
+  	
+  	checkSides();
+
+  	// check and remove blocks necessary
+  	checkHit();
+
+  	// reset variables for the new column and move the current row to +1
+  	if (remaining>0) {
+  		currRow++;
+  		currCol=0;
+  		togo=0;
+  	}
+  	else {
+  		console.log("no blocks remaining");
+  	}
   	// setTimeout(function, 500); // waits for half a second, to prevent spamming
   }
 }, false);
 
 // initializes our refresh rate and starts the game
 function init() {
+	// set the array to empty
+	for (var i=0; i<saved.length;i++) {
+		saved[i]=0;
+	}
  	// 200ms or 5 fps
  	refresh = setInterval(updateGame, speed);
 }
@@ -74,9 +94,19 @@ function drawArray() {
 	// Run through each element in our array
 	for (var i = 0; i < saved.length; i++) {
 		if (saved[i]==1) {
-			console.log("Printing at "+i);
+			//console.log("Printing at "+i);
 			if (canvas.getContext) {
 				ctx.fillStyle = "rgb(0,120,255)";
+				// x position from left, y position from top, width, height
+				var x = (i%width) * blockWidth;
+				var y = (height*blockHeight)-blockHeight - (Math.floor(i/width) * blockHeight); // Distance from top - [ row * block height ]
+				ctx.fillRect (x, y, blockWidth, blockHeight);
+				ctx.strokeRect(x, y, blockWidth, blockHeight);
+			}
+		}
+		else {
+			if (canvas.getContext) {
+				ctx.fillStyle = "rgb(31,59,115)";
 				// x position from left, y position from top, width, height
 				var x = (i%width) * blockWidth;
 				var y = (height*blockHeight)-blockHeight - (Math.floor(i/width) * blockHeight); // Distance from top - [ row * block height ]
@@ -89,13 +119,13 @@ function drawArray() {
 
 function setArray() {
 	if ( !reverse ) {
-		console.log("currCol="+currCol);
+		//console.log("currCol="+currCol);
 		// if we're in a column that is going to force us to fill all blocks
 		if ( currCol<remaining-1 ) {
-			for ( var i=0; i<=togoleft;i++) {
+			for ( var i=0; i<=togo;i++) {
 				saved[(currRow*width)+i] = 1;
 			}
-			togoleft++;
+			togo++;
 			currCol++;
 		}
 		// if we're in a middle column, where we just fill the previous remaining-1 blocks
@@ -103,7 +133,9 @@ function setArray() {
 			for ( var i=0; i<remaining; i++) {
 				// Each time we have to update up to 3 blocks
 				saved[(currRow*width)+currCol-i] = 1;
-				saved[(currRow*width)+currCol-1-i] = 0; // draws empty over the previous placement
+				if ( (currRow*width)+currCol-1-i >= (currRow*width) ) {
+					saved[(currRow*width)+currCol-1-i] = 0; // draws empty over the previous placement
+				}
 			}
 			currCol++;
 		}
@@ -112,133 +144,151 @@ function setArray() {
 			for ( var i=0; i<=togo; i++) {
 				// Each time we have to update up to 3 blocks
 				saved[(currRow*width)+currCol-i] = 1;
-				saved[(currRow*width)+currCol-1-i] = 0; // draws empty over the previous placement
+				if ( (currRow*width)+currCol-1-i >= (currRow*width) ) {
+					saved[(currRow*width)+currCol-1-i] = 0; // draws empty over the previous placement
+				}
 			}
 			if ( togo==0 ) {
 				reverse = true;
-				//currCol--;
-				console.log("Now reversing");
+				
+				currCol--; // This will put us back 1 column, so we don't print twice at the final row (will look smoother)
+				//saved[(currRow*width)+width-1] = 0;
+				//console.log("Now reversing");
 			}
 			if ( togo!=0) {
 				togo--;
 			}
 		}
 	}
-	// we're going backwards now, so we mirror the previous code exactly
+	// we're going backwards now and will start at width-2 (prevents looking like we're stopping at the end)
 	else {
-		console.log("we're here");
-		// if we're in the last column
-		if ( currCol==width-1 && togo<remaining) {
-			console.log("in the end, now printing back");
-			// count back from togo
-			for ( var i=0; i<togo; i++) {
-				saved[(currRow*width)+currCol-i] = 1;
-				saved[(currRow*width)+currCol-1-i] = 0; // draws empty over the previous placement
+		//console.log("Reversing now, togo="+togo);
+		//console.log("we're here");
+		// if we're in column where we need to fill all blocks on the rhs
+		if ( currCol==width-2 ) {
+			//console.log("in the end, now printing back");
+			// loop through the remaining blocks
+			for ( var i=0; i<remaining; i++) {
+				// start printing blocks at currCol and then go currCol++ as long as they don't go into the next row
+				if ( ((currRow*width)+currCol+i)<= ((currRow*width)+width-1) ) {
+					saved[(currRow*width)+currCol+i] = 1;
+					// before we set any values to 0, make sure they are in the current row only
+					if ( (currRow*width)+currCol+1+i <= ((currRow*width)+width-1) ) {
+						saved[(currRow*width)+currCol+1+i] = 0; // draws empty over the previous placement
+					}
+				}
 			}
-			togo++;
 			currCol--;
+			//console.log("--> currCol="+currCol);
+			togo = remaining-1;
 		}
-		
-	}
-}
+		// We're in a middle section
+		else if ( currCol < (width-2) && currCol!=0 ) {			
+			//console.log("this is a middle section");
 
-// This function sets the values of the 2D array "saved"
-// This function is updated at the start of each execution of the main update function
-function setArrayOriginal() {
-	console.log(currCol);
-	// check blocks remaining
-
-	if ( !reverse) {
-		// Check if we're at the beginning
-		if ( currCol<remaining) {
-			// We need to increment the togo blocks
-			for (var i=0; i<=togoleft;i++) {
-				console.log("Column "+currCol+" togoleft="+togoleft);
-				saved[(currRow*width)+i] = 1;
-			}
-			togoleft++;
-			currCol++;
-		}
-		// If we're in the middle, but not at the final column
-		else if ( currCol<width-1 ) {
-			if (currCol==1)
-				console.log("NOW");
 			for ( var i=0; i<remaining; i++) {
 				// Each time we have to update up to 3 blocks
-				saved[(currRow*width)+currCol-i] = 1;
-				saved[(currRow*width)+currCol-1-i] = 0; // draws empty over the previous placement
+				saved[(currRow*width)+currCol+i] = 1;
+				if ( (currRow*width)+currCol+1+i <= ((currRow*width)+width-1) ) {
+					saved[(currRow*width)+currCol+1+i] = 0; // draws empty over the previous placement
+				}
 			}
-			currCol++;
+			currCol--;
+			if (currCol==0) {
+				togo++;
+			}
 		}
-		// At the end, now reverse
+		// we're in the first column
 		else {
-			console.log("At the end");
-			for ( var i=0; i<=togo; i++) {
+			//console.log("IN FIRST ROW-TOGO="+togo);
+			//console.log("**ELSE**");
+
+			for ( var i=0; i<togo; i++) {
 				// Each time we have to update up to 3 blocks
-				saved[(currRow*width)+currCol-i] = 1;
-				saved[(currRow*width)+currCol-1-i] = 0; // draws empty over the previous placement
+				saved[(currRow*width)+currCol+i] = 1;
+				if ( (currRow*width)+currCol+1+i <= ((currRow*width)+width-1) ) {
+					saved[(currRow*width)+currCol+1+i] = 0; // draws empty over the previous placement
+				}
 			}
-			
-			if ( togo==0 ) {
-				reverse = true;
-				togorev = remaining;
-				//currCol--;
-				console.log("Now reversing");
-			}
-			if ( togo!=0) {
+
+			if ( togo>0) {
 				togo--;
 			}
-		}
-	}
-	else if ( reverse) {
-		if ( currCol!=0 && currCol>remaining ) {
-			// We have to check the far ends
-			console.log("currCol="+currCol+" togo="+togo);
-			if ( currCol==width-1 && togo!=2 ) {
-				// At the right side here
-				console.log("Now counting them back");
-				togo++;
-				for ( var i=togo; i>0; i-- ) {
-					saved[(currRow*width)+currCol-i] = 1;
-					saved[(currRow*width)+currCol+1-i] = 1;
+			// We'll check how many we have to draw
+			if ( togo==0) {
+				reverse = false; // we can reverse now
+				
+				// this will put us into column 1, so that we don't delay while in column 0
+				if (remaining>1) {
+
+					togo++;
 				}
-			}
-			else {
-				for ( var i=remaining; i>0; i--) {
-					saved[(currRow*width)+currCol-i] = 1;
-					saved[(currRow*width)+currCol+1-i] = 0;
-				}
-				currCol--;
-			}
-		}
-		// At the beginning again
-		else if ( currCol!=0 && currCol<=togoleft ) {
-			console.log("currCol="+currCol+" printing in :");
-			for (var i=0; i<=togoleft;i++) {
-				console.log("Column "+currCol+" togoleft="+togoleft);
-				saved[(currRow*width)+i] = 1;
-			}
-			togoleft--;
-			currCol--;
-		}
-		else {
-			console.log("currCol=0");
-				// We're at the first column now
-				reverse = false;
-				//saved[(currRow*width)+currCol] = 1;
-				//saved[(currRow*width)+currCol+1] = 0;
 				currCol++;
+			}
 		}
 	}
 }
 
-function checkCollision() {
-	// We have to check if the blocks just placed are on top of the previous row
-	// Discard all blocks that are not on top of a block in the previous row
+/*
+this just loops through the current row, finding all indices==1 and then comparing it with the one
+directly below it (i-width) and determining whether we need to remove a block
+*/
+function checkHit() {
+	// make sure this isn't the first row
+	if (currRow!=0) {
+		for (var i = (currRow*width); i < ((currRow*width)+width); i++) {
+			console.log("At pos="+i);
+			console.log("saved[i]="+saved[i]+"saved[i-width]="+saved[i-width]);
+
+			// if the indice is 1 and there is no block underneath it, set it to 0
+			if ( saved[i]==1 && saved[i-width]==0) {
+				saved[i]=0;
+				remaining--;
+
+				if ( remaining==0 ) {
+					console.log("GAME OVER");
+				}
+			}
+		}
+	}
+}
+
+// the blocks will go into the sides and have to be accounted for if they are stopped while in there
+function checkSides() {
+	if (currRow==0) {
+  		for ( var i=0; i<width; i++ ) {
+  			if ( saved[i]==1 ) {
+  				temp++;
+  			}
+  		}
+  		remaining = temp;
+  		temp = 0;
+  	}
+  	else if ( currRow > 0) {
+  		for ( var i=(currRow*width); i<(currRow*width)+width; i++ ) {
+  			if ( saved[i]==1 ) {
+  				temp++;
+  			}
+  		}
+  		remaining = temp;
+  		temp = 0;
+  	}
+}
+
+function checkWin() {
+	if (currRow==height-1) {
+		console.log("You win!");
+	}
 }
 
 function updateGame() {
- 	setArray();
-	// Always draw our saved blocks first
-	drawArray();
+	if ( remaining > 0 && currRow < height ) {
+		// set blocks first and then print them
+		setArray();
+		drawArray();
+		checkWin();
+	}
+	else {
+		console.log("the game is now over");
+	}
 }
